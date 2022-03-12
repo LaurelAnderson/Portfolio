a514c48dd58a6f1de9a308456f638ee64fd19fe3
LaurelAnderson <laureloanderson@gmail.com>
2022-03-11 20:07:04 -0800

    Add cards to projects page. Need to get modal working

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import '../homeStyle.css'
+import Headshot from '../img/laurel3bw.jpg'
 
 function About() {
   return (
     <div className="home">
       <div class="container">
+
+        {/* first content */}
         <div className="content">
-          <h1>About Me</h1>
           <div class="row">
+            <div class="col">
+              <h1>Headline 1</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div class="col">
+              <img src={Headshot} class="img-fluid" alt="Laurel Anderson"/>
+            </div>
+          </div>
+        </div>
+
+        {/* second content */}
+        <div className="content">
+          <h1>Headline 2</h1>
+          <div class="row">
+            <div class="col">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
             <div class="col">
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -20,6 +43,7 @@ function About() {
             </div>
           </div>
         </div>
+
       </div>
     </div>
   );
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,13 +1,42 @@
 import React from "react";
+import Test from "../img/test.jpg"
 
 function Projects() {
   return (
     <div className="projects">
       <div class="container">
         <h1>Projects</h1>
-        <p>
-          this is the projects page
-        </p>
+
+        {/* Put this in a separate component */}
+        <div class="card" style={{width: '18rem'}}>
+          <div class="card-body">
+            This is some text within a card body.
+          </div>
+          <img src={Test} class="card-img-bottom" alt="Price with some cats" data-bs-toggle="modal" data-bs-target="#exampleModal"/>
+
+          {/* Modal */}
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </div>
   );
