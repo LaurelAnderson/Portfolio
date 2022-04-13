f274ba6adc86407f1b1f88db7735dd8218ac341d
LaurelAnderson <laureloanderson@gmail.com>
2022-04-12 18:51:41 -0700

    change nav style

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -6,23 +6,23 @@ function Home() {
   return (
     <div className="home">
       <div className="background1">
-        <div class="container px-5 ">
+        <div class="container px-5">
 
+        <br></br>
         <br></br>
       
           <h1>Well Hello There, </h1>
 
           <br></br>
+          <br></br>
 
-
-          {/* <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3"> */}
-          <div class="row row-cols-1 row-cols-md-2"> 
+          <div class="row row-cols-1 row-cols-lg-2"> 
             <div className="col">
-              <div>{/* Empty */}</div>
+              <div className="test1">{/* Empty */}</div>
             </div>
             <div className="col">
-              <div className="test2">
-                <h3 className="p-4">
+              <div className="test1">
+                <h3 className="p-4 m-3">
                 I am a creative and ambitious computer scientist, passionate about 
                 problem solving, team collaboration, and professional communication.
                 Thank you for visiting my site!
@@ -35,9 +35,10 @@ function Home() {
       </div>
 
       <br></br>
+      <br></br>
 
       <div className="background2">
-        <div class="container px-5 ">
+        <div class="container px-5">
           {/* Hightlights */}
 
           <h1>Highlights</h1>
