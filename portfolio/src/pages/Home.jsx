11f3d7cb6a64690365132ab1198a98e587d4a1ff
LaurelAnderson <laureloanderson@gmail.com>
2022-04-12 18:15:18 -0700

    add styling for home page with cards

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import Card from "../components/Card.jsx"
+import Test from "../img/test.jpg";
 
 function Home() {
   return (
     <div className="home">
       <div className="background1">
-        <div class="container">
-          
-          <div className="inner-container">
-            <h1>Well Hello There, </h1>
+        <div class="container px-5 ">
 
-            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2">
-              <div class="col">
-                {/* Empty */}
-              </div>
-              <div class="col">
-                <h4>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </h4>
+        <br></br>
+      
+          <h1>Well Hello There, </h1>
+
+          <br></br>
+
+
+          {/* <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3"> */}
+          <div class="row row-cols-1 row-cols-md-2"> 
+            <div className="col">
+              <div>{/* Empty */}</div>
+            </div>
+            <div className="col">
+              <div className="test2">
+                <h3 className="p-4">
+                I am a creative and ambitious computer scientist, passionate about 
+                problem solving, team collaboration, and professional communication.
+                Thank you for visiting my site!
+                </h3>
               </div>
             </div>
           </div>
@@ -24,9 +34,26 @@ function Home() {
         </div>
       </div>
 
+      <br></br>
+
       <div className="background2">
-        <div class="container">
+        <div class="container px-5 ">
           {/* Hightlights */}
+
+          <h1>Highlights</h1>
+          <br></br>
+          <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3">
+            <div class="col">
+              <Card imgSrc={Test} cardBody="This is some text within a card body." text="Hello,you're reading this text in a modal!"/>
+            </div>
+            <div class="col">
+              <Card imgSrc={Test} cardBody="This is some text within a card body." text="Hello,you're reading this text in a modal!"/>
+            </div>
+            <div class="col">
+              <Card imgSrc={Test} cardBody="This is some text within a card body." text="Hello,you're reading this text in a modal!"/>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
